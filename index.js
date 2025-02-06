75dd4f8987e39b97a57b5d3c84249d89cfd38e27
akashofficial012 <akashmondal.code@gmail.com>
2025-02-06 18:46:07 +0600

    connect postgres database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
 
 dotenv.config();
 
@@ -8,11 +9,26 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
+
+const sequelize = new Sequelize('authentication', 'postgres', 'Akash123', {
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  })
+async function connectDb() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await connectDb();
     console.log(`Server is running on port ${port}`);
 });
